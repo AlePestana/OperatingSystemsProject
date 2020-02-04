937d2bdc052a14c44adff781dce056836ae0f7a8
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-02-03 18:02:06 -0600

    Lectura de datos en cada comando

--- main.cpp
+++ main.cpp
@@ -15,20 +15,41 @@ int main(){
         datos >> comando;
         switch(comando|32){
             //Accesar dirección virtual
-            case 'a': break;
+            case 'a': 
+                if(datos >> dato1 >> dato2 >> dato3)
+
+            break;
+
             //Comentario
             case 'c': 
-            if(linea.length() > 2)
-            cout << linea.substr(2) << "\n"; 
+                if(linea.length() > 2)
+                cout << linea.substr(2) << "\n";
             break;
+
             //Fin (devolver turnaround promedio, num de page faults, num total de swap-in y swap-out)
-            case 'f': break;
+            case 'f': 
+                
+
+            break;
+
             //Exit
-            case 'e': break;
+            case 'e': 
+            
+            break;
+
             //Liberar páginas de un proceso
-            case 'l': break;
+            case 'l': 
+                datos >> dato1;
+                //Liberar memoria en dato1
+
+            break;
+            
             //Cargar un proceso
-            case 'p': break;
+            case 'p': 
+                if(datos >> dato1 >> dato2)
+                    
+            break;
+            
             //Ignorar linea en caso default
         }
     }
